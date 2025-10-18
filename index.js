8a5056430f2f588b9e0c76bcd9b8ecb1010c759e
kaytipooficial <jesusjimenezcarbajo@gmail.com>
2025-10-18 21:51:36 +0200

    Aa

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname));
+// Servir archivos estáticos
+app.use('/pages', express.static(path.join(__dirname, 'pages')));
+
+// Ruta principal que genera links automáticamente
+app.get('/', (req, res) => {
+    const folderPath = path.join(__dirname, 'pages');
+    const files = fs.readdirSync(folderPath)
+                    .filter(file => file.endsWith('.html'));
+
+    let html = `<h1>Página Principal</h1><ul>`;
+    files.forEach(file => {
+        html += `<li><a href="/pages/${file}">${file.replace('.html','')}</a></li>`;
+    });
+    html += `</ul>`;
+    res.send(html);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
